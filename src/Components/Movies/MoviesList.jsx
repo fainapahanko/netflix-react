cb72d5c9e131b7b5bd5ce2667c8de2176b805886
Faina Poganko <fainapoganko@MacBook-Air-Faina.local>
2019-11-27 17:18:27 +0100

    loaders

--- src/Components/Movies/MoviesList.jsx
+++ src/Components/Movies/MoviesList.jsx
@@ -3,22 +3,46 @@ import ListFilms from '../Movies/ListFilms';
 import SearchedFilms from '../Movies/SearchedFilms';
 import { TraceSpinner } from "react-spinners-kit";
 import{InputGroup, InputGroupAddon, InputGroupText, Input, Row} from 'reactstrap'
+import '../../index.css'
 
 var arr = ["Star Wars","Harry Potter","Air Bud"]
 
 class MoviesList extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            movies: [],
-            selectedMovie: undefined,
-            searchResult: '',
-            films: [],
-            loading: true,
-        };
+    state = {
+        movies: [],
+        selectedMovie: undefined,
+        searchResult: '',
+        films: [],
+        loading: true,
     }
 
-    componentDidMount = () => {
+    render(){
+        console.log(this.state.movies)
+        return(
+            <>
+                <InputGroup>
+                    <InputGroupAddon addonType="prepend">
+                            <InputGroupText>Search</InputGroupText>
+                    </InputGroupAddon>
+                    <Input 
+                        onChange={this.searchMovie}
+                        placeholder="harry potter" />
+                </InputGroup>
+                {this.state.loading && <div className="col-md-12 justify-content-center align-items-center">
+                        <TraceSpinner size={30} color="#73C991"/>
+                    </div>}
+                <Row>
+                    {this.state.films && this.state.films
+                        .map((movie, index) => <SearchedFilms onSelected={this.onSelectedFilm} film={movie} key={index}/>)}
+                </Row>
+                <Row>
+                    {this.state.movies && this.state.movies.map((film, index) => <ListFilms movie={film} key={index}/>)
+                }
+                </Row>
+            </>
+        )
+    }
+    componentDidMount = async () => {
         this.setState({
             loading: true
         })
@@ -26,16 +50,16 @@ class MoviesList extends React.Component {
         arr.forEach(async(element) => {
             let response = await fetch("http://www.omdbapi.com/?apikey=ad6a24df&s=" + element)
             filmsInfo = await response.json();
-            console.log(filmsInfo)
-            await this.setState({
+            this.setState({
                 movies: [...this.state.movies, {
                     title: element,
-                    info: filmsInfo.Search
+                    info: filmsInfo.Search,
                 }],
-                loading: false,
+                loading: false
             })
         })
     }
+
 
     searchMovie = async(ev) => {
         let input = ev.target.value 
@@ -43,39 +67,8 @@ class MoviesList extends React.Component {
         let movies = await response.json()
         this.setState({
             searchResult: input,
-            films: movies.Search
+            films: movies.Search,
         })
-    }
-
-    render(){
-        let movies = this.state.movies
-
-        if(this.state.loading){  
-            return <div className="col-md-12 justify-content-center align-items-center">
-                <TraceSpinner size={30} color="#73C991"/>
-            </div>
-        }
-        return(
-            <>
-                <InputGroup>
-                    <InputGroupAddon addonType="prepend">
-                            <InputGroupText>Search</InputGroupText>
-                        </InputGroupAddon>
-                        <Input 
-                        onChange={this.searchMovie}
-                        placeholder="harrypotter" />
-                    </InputGroup>
-                <Row>
-                    {this.state.films && this.state.films
-                        .map((movie, index) => <SearchedFilms onSelected={this.onSelectedFilm} film={movie} key={index} />
-                    )}
-                </Row>
-                <Row>
-                    {!movies ? <TraceSpinner size={30} color="#73C991"/> : movies.map((film, index) => <ListFilms onSelected={this.onSelectedFilm} movie={film} key={index}/>
-                        )}
-                </Row>
-            </>
-        )
     }
 }
 
--- src/Components/Movies/SingleFilm.jsx
+++ src/Components/Movies/SingleFilm.jsx
@@ -4,7 +4,6 @@ import '../../index.css'
 import {Link} from 'react-router-dom'
 
 const SingleFilm = props => {
-  console.log(props)
     return(
         <Card className="col-lg-3 col-md-4 col-sm-6 col-12">
           <Link to={"/details" + props.film.imdbID}>
